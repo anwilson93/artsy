6247f4d97047437d6dd902e5d5507de6bed30f56
Angelica <angelica.wilson93@gmail.com>
2020-12-30 13:29:29 -0600

    Set up backend route for view product

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const { ImageUrl, ArtProduct } = require('../../db/models');
+const { ImageUrl, ArtProduct, Shop } = require('../../db/models');
 const asyncHandler = require("express-async-handler");
 const router = express.Router();
+
+router.get(`/:id(\\d+)`, asyncHandler(async (req, res) => {
+  const id = req.params.id
+  const artProduct = await ArtProduct.findByPk(id, {
+    include: Shop
+  });
+  return res.json({artProduct})
+}));
 
 router.get('/', asyncHandler(async (req, res) => {
   const artProducts = await ImageUrl.findAll({
@@ -9,5 +17,8 @@ router.get('/', asyncHandler(async (req, res) => {
   });
   return res.json({artProducts})
 }));
+
+
+
 
 module.exports = router;
--- frontend/src/components/ViewProduct/index.js
+++ frontend/src/components/ViewProduct/index.js
@@ -1,11 +1,28 @@
 import {useParams} from "react-router-dom";
+import {useEffect} from 'react';
+import {fetchAllProducts} from '../../store/artProducts';
+import {useDispatch, useSelector} from 'react-redux';
+import AddToCart from "./AddToCart";
 
-const ViewProduct = ({}) => {
+const ViewProduct = () => {
     const {id} = useParams();
+
+
+    const dispatch = useDispatch()
+    const currentArtProducts = useSelector(fullReduxState => {
+        return fullReduxState.artProducts
+    });
+
+    useEffect (() => {
+        dispatch(fetchAllProducts())
+    }, [dispatch])
+
+    const artProduct = currentArtProducts.find(art => art.artProductId === Number(id));
+    console.log(artProduct)
     return (
         <div>
-            
-            <h1>{id}</h1>
+            {/* <img style={{width: 500, height: 600, marginTop: 50, marginLeft: 300}} src ={artProduct.url} key={artProduct.artProductId} alt='' />
+            <AddToCart artProduct={artProduct} /> */}
         </div>
        
     )
