8b9eb86b35ec1e2b29111ffa8d80aa9d50cecd13
Angelica <angelica.wilson93@gmail.com>
2021-02-07 19:40:06 -0600

    heroku

--- backend/config/database.js
+++ backend/config/database.js
@@ -15,14 +15,14 @@ module.exports = {
     dialect: "postgres",
     seederStorage: "sequelize",
   },
-  // production: {
-  //   use_env_variable: "DATABASE_URL",
-  //   dialect: "postgres",
-  //   seederStorage: "sequelize",
-  // },
-  "production": {
-  "dialect": "postgres",
-  "seederStorage": "sequelize",
-  "use_env_variable": "DATABASE_URL"
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  // "production": {
+  // "dialect": "postgres",
+  // "seederStorage": "sequelize",
+  // "use_env_variable": "DATABASE_URL"
 }
 };
