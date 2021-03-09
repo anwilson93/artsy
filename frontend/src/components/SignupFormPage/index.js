6dd2a35bd85a5eec58ebf3d571161b1cfcc8cc78
Angelica <angelica.wilson93@gmail.com>
2021-03-08 22:56:17 -0600

    Styled registration page

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import './SignupForm.css';
+import HomePageJumbotron from '../HomePageJumbotron';
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -29,51 +30,59 @@ function SignupFormPage() {
 
   return (
     <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
+      <HomePageJumbotron />
+      <div className='login-form-container register-form-container'>
+        <div className='login-form-contents'>
+          <div className='registration-form-title-header'>
+            <h2 id='login-form-title'>Create your account</h2>
+            <p className='registration-text'>Registration is easy.</p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <ul>
+              {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+            <div className='login-form-inputs-container'>
+            <label className='login-form-label'>Email</label>
+            <input
+              className='login-form-input'
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label className='login-form-label'>Username</label>
+            <input
+              className='login-form-input'
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <label className='login-form-label'>Password</label>
+            <input
+              className='login-form-input'
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label className='login-form-label'>Confirm Password</label>
+            <input
+              className='login-form-input'
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            <button type="submit" className='login-form-submit-button'>Register</button>
+          </div>
+        </form>
+      </div>
+    </div>
     </>
   );
 }
+     
+     
 
 export default SignupFormPage;
