dd8ea601cfcd4ea64e7059928778df8449bbfc80
Angelica <angelica.wilson93@gmail.com>
2021-03-09 17:07:21 -0600

    Changed style of product page a bit

--- frontend/src/components/ViewProduct/AddToCart.js
+++ frontend/src/components/ViewProduct/AddToCart.js
@@ -5,7 +5,8 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import * as sessionActions from "../../store/session";
 import './ViewProduct.css';
-import { applyMiddleware } from 'redux';
+import inStock from './icons8-checkmark-26.png'
+
 
 
 const AddToCart = ({currentArtProduct}) => {
@@ -50,20 +51,34 @@ const AddToCart = ({currentArtProduct}) => {
     // MAKES LOGIN FORM MODAL APPEAR. NEED TO CHANGE!
     if(!user){
         return (
-        <div className='product-info'>
-            <div>
-                <Link to={`/shops/${currentArtProduct.Shop.id}`}>
-                    {currentArtProduct.Shop.shopName}
-                </Link>
-            </div>
+            <div className='product-info'>
             <div id='product-title'>
                 {currentArtProduct.title}
             </div>
-              <div id='product-price'>
-                ${currentArtProduct.price}
+            <div id='product-description'>
+                Description: {currentArtProduct.description}
             </div>
+            <div id='product-price-container'>
+                <div id='product-price'>
+                    ${currentArtProduct.price}
+                </div>
+                <div id='product-in-stock'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                    </svg>
+                    In Stock
+                </div>
+            </div>
+            <div className='shipping-truck'>
+                <svg xmlns="http://www.w3.org/2000/svg" width='48' height="48" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                    <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                </svg>
+                <div className='shipping-truck-text'>
+                    <span style={{fontWeight: "bold"}}>Hooray!</span> This item ships free to the US.
+                </div>
+                </div>
             <div>
-                <button onClick={() => setShowModal(true)}>Add To Cart</button>
+                <button className='add-to-cart-button' onClick={() => setShowModal(true)}>Add To Cart</button>
                 {showModal && (
                     <Modal onClose={() => setShowModal(false)}>
                         <div class='login-form-container'>
@@ -109,32 +124,49 @@ const AddToCart = ({currentArtProduct}) => {
                     </Modal>
                 )}
             </div>
-              <div id='product-description'>
-                Description: {currentArtProduct.description}
-            </div>
         </div>
-    )
+        )
         
     } else {
 
     return (
         <div className='product-info'>
-            <div>
+            {/* <div className='shop-title'>
                 <Link to={`/shops/${currentArtProduct.Shop.id}`}>
                     {currentArtProduct.Shop.shopName}
                 </Link>
-            </div>
+            </div> */}
             <div id='product-title'>
                 {currentArtProduct.title}
             </div>
-              <div id='product-price'>
-                ${currentArtProduct.price}
-            </div>
-            <div>
-                <button onClick={addItemToCart}>Add To Cart</button>
-            </div>
-              <div id='product-description'>
+            <div id='product-description'>
                 Description: {currentArtProduct.description}
+            </div>
+            <div id='product-price-container'>
+                <div id='product-price'>
+                    ${currentArtProduct.price}
+                </div>
+                <div id='product-in-stock'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                    </svg>
+                    In Stock
+                    {/* <img
+                        key='in-stock'
+                        src={inStock} alt='in stock' /> 
+                    In Stock */}
+                </div>
+            </div>
+            <div className='shipping-truck'>
+                <svg xmlns="http://www.w3.org/2000/svg" width='48' height="48" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                    <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                </svg>
+                <div className='shipping-truck-text'>
+                    <span style={{fontWeight: "bold"}}>Hooray!</span> This item ships free to the US.
+                </div>
+                </div>
+            <div>
+                <button className='add-to-cart-button' onClick={addItemToCart}>Add To Cart</button>
             </div>
         </div>
     )
