b3acdab6f202f30477a3e85dbbdf75d12da60660
Angelica <angelica.wilson93@gmail.com>
2021-03-11 19:19:21 -0600

    Work on restyling my reviews page

--- frontend/src/components/MyReviews/index.js
+++ frontend/src/components/MyReviews/index.js
@@ -2,6 +2,8 @@ import {useDispatch, useSelector} from 'react-redux';
 import {useEffect} from 'react';
 import {fetchAllUserReviews, deleteReview} from '../../store/artProductReviews.js';
 import {Link} from 'react-router-dom';
+import HomePageJumbotron from '../HomePageJumbotron/index.js';
+import './MyReviews.css';
 
 
 const MyReviews = () => {
@@ -21,20 +23,28 @@ const MyReviews = () => {
 
     if(!sessionUser){
         return (
-            <h3>You must be logged in to view your Reviews!</h3>
+            <>
+            <HomePageJumbotron />
+            <h3 className='add-margin'>You must be logged in to view your Reviews!</h3>
+            </>
         )
     }
 
     if(myReviews.length < 1) {
         return (
-            <h3>You have no reviews</h3>
+            <>
+            <HomePageJumbotron />
+            <h3 className='add-margin'>You have no reviews</h3>
+            </>
         )
     }
 
     return (
         <>
-            <h3>All Reviews</h3>
+            <HomePageJumbotron />
+            <h3 className='add-margin'>All Reviews</h3>
             {myReviews && myReviews.map(review => {
+
                 const deleteReviewButton = (e) => {
                     e.preventDefault();
                     dispatch(deleteReview(review.id, sessionUser.id));
@@ -42,13 +52,14 @@ const MyReviews = () => {
                
             return (
                 <>
-                    
+                    <div className='my-reviews-container'>
                     <Link to={`/products/${review.artProductId}`} id='link'>{review.ArtProduct.title} Review
                     </Link>
                     <form style={{marginBottom: 50}}>
                         <p>{review.User.username} said: {review.review}</p>
                         <button onClick={deleteReviewButton}>Delete This Review</button>
                     </form>
+                    </div>
                 </>
             )
         })}
